842fb36c189fb1d722ab63b463a3b48bafa70cf5
ousef777 <yousef.alfeeli@gmail.com>
2024-11-14 14:09:56 +0300

    changed default image

--- Book.js
+++ Book.js
@@ -4,7 +4,7 @@ const BookSchema = new Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
     price: { type: Number, default: 5 },
-    image: { type: String, default: "./media/book-placeholder.png" },
+    image: { type: String, default: "localhost:8000/media/book-placeholder.png" },
 });
 
 module.exports = model('Book', BookSchema);
